098f74949b2e6709459a446c2236b9e97e222eca
Srivarshan-T <140912234+Srivarshan-T@users.noreply.github.com>
2024-02-10 19:12:48 +0530

    Add files via upload

---
+++ gmaillogin.js
@@ -0,0 +1,20 @@
+function checkgmail(){
+    let gmail=document.getElementById("inputbox").value;
+    
+   if(gmail.endsWith("@gmail.com")){
+    
+     document.getElementById("error").style.visibility="collapse"; 
+     document.getElementById("inputbox").style.outline="none";
+     document.getElementById("errormsg").style.visibility="collapse";
+     document.getElementById("succmsg").style.visibility="visible";
+    
+   } 
+   else{
+     document.getElementById("error").style.visibility="visible"; 
+     document.getElementById("inputbox").style.outline="solid red";
+     document.getElementById("errormsg").style.visibility="visible";
+     document.getElementById("succmsg").style.visibility="collapse";
+   }
+
+
+}
